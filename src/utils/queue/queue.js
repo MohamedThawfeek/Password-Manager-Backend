36509baa963f758a593a39c00543cde7620dc271
thawfeek <mohamedthawfeek96@gmail.com>
2026-01-16 12:02:39 +0530

    new changes

--- src/utils/queue/queue.js
+++ src/utils/queue/queue.js
@@ -1,19 +1,19 @@
 // utils/emailQueue.js
-// const PQueue = require("p-queue").default
+const PQueue = require("p-queue").default
 
-// // Concurrency: 1 ensures one email send at a time
-// const emailQueue = new PQueue({ concurrency: 1 });
+// Concurrency: 1 ensures one email send at a time
+const emailQueue = new PQueue({ concurrency: 1 });
 
-// module.exports = emailQueue;
+module.exports = emailQueue;
 
 
-let emailQueue;
+// let emailQueue;
 
-(async () => {
-  const PQueue = (await import('p-queue')).default;
-  emailQueue = new PQueue({ concurrency: 1 });
-})();
+// (async () => {
+//   const PQueue = (await import('p-queue')).default;
+//   emailQueue = new PQueue({ concurrency: 1 });
+// })();
 
-module.exports = {
-  getQueue: () => emailQueue,
-};
+// module.exports = {
+//   getQueue: () => emailQueue,
+// };
